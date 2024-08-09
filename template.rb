fa1aed7877dd9ab9a2a574bafc2bff83db13d76f
Lehi Sanchez <17449+lehisanchez@users.noreply.github.com>
2024-08-09 19:19:44 -0400

    Updates

--- template.rb
+++ template.rb
@@ -2,18 +2,20 @@
 # Gems
 # =========================================================
 gem_group :development, :test do
-  gem "faker", require: false
   gem "rspec-rails", require: false
 end
 
 gem_group :test do
-  gem "capybara"
-  gem "selenium-webdriver"
   gem "shoulda-matchers"
 end
 
 gem "devise"
 gem "high_voltage"
+
+# =========================================================
+# README
+# =========================================================
+run 'rm README.md'
 
 # =========================================================
 # RSpec
@@ -29,8 +31,6 @@ after_bundle do
   rails_command("generate devise:install")
 end
 
-environment 'config.action_mailer.default_url_options = { host: "localhost", port: 3000 }', env: 'development'
-
 # =========================================================
 # High Voltage
 # =========================================================
@@ -44,10 +44,39 @@ CODE
 file 'app/views/pages/home.html.erb'
 
 # =========================================================
+# YJIT
+# =========================================================
+initializer 'enable_yjit.rb', <<-CODE
+  if defined? RubyVM::YJIT.enable
+    Rails.application.config.after_initialize do
+      RubyVM::YJIT.enable
+    end
+  end
+CODE
+
+# =========================================================
+# Enable UUID
+# =========================================================
+after_bundle do
+  generate(:migration, "EnableUUID")
+
+  inject_into_file Dir.glob("db/migrate/*_enable_uuid.rb").first, after: "def change\n" do <<-CODE
+    enable_extension 'pgcrypto'
+  CODE
+  end
+end
+
+inject_into_file 'config/application.rb', after: "config.eager_load_paths << Rails.root.join(\"extras\")\n" do <<-CODE
+  config.generators do |g|
+    g.orm :active_record, primary_key_type: :uuid
+  end
+CODE
+end
+
+# =========================================================
 # Database
 # =========================================================
 after_bundle do
-  # rails_command("db:drop")
   rails_command("db:create")
   rails_command("db:migrate")
   rails_command("db:seed")
@@ -57,10 +86,10 @@ end
 # GIT
 # =========================================================
 inject_into_file '.gitignore' do
-  "\n# Hidden system files\n.DS_Store"
+  "\n# Hidden system files\n.DS_Store\n"
 end
 
 after_bundle do
   git add: "."
-  git commit: %Q{ -m 'Initial commit' }
+  git commit: %Q( -m 'Initial commit' )
 end
