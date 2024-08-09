a90b2113af239d89a1b654335c4ee6317d9df36e
Lehi Sanchez <17449+lehisanchez@users.noreply.github.com>
2024-08-09 08:13:34 -0400

    Updates

--- template.rb
+++ template.rb
@@ -1,71 +1,39 @@
-# Delete default Gemfile
-run 'mv Gemfile Gemfile.example'
-
-# Create Gemfile
-file 'Gemfile', <<-CODE
-source "https://rubygems.org"
-
-ruby file: ".ruby-version"
-
-gem "rails", github: "rails/rails"
-
-# Drivers
-gem "pg", "~> 1.1"
-gem "redis", ">= 4.0.1"
-
-# Deployment
-gem "puma", ">= 5.0"
-
-# Front-end
-gem "propshaft"
-gem "importmap-rails"
-gem "turbo-rails"
-gem "stimulus-rails"
-gem "high_voltage"
-
-# Authentication
-gem "devise"
-
-# Other
-gem "bootsnap", require: false
-gem "kamal", require: false
-gem "thruster", require: false
-
-group :development do
-  gem "web-console"
-end
-
-group :development, :test do
-  gem "brakeman", require: false
-  gem "debug"
+# =========================================================
+# Gems
+# =========================================================
+gem_group :development, :test do
   gem "faker", require: false
   gem "rspec-rails", require: false
-  gem "rubocop-rails-omakase", require: false
 end
 
-group :test do
+gem_group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem "shoulda-matchers"
 end
-CODE
 
-# Bundle install
-run 'bundle install'
+gem "devise"
+gem "high_voltage"
 
+# =========================================================
 # RSpec
-rails_command("generate rspec:install")
+# =========================================================
+after_bundle do
+  rails_command("generate rspec:install")
+end
 
+# =========================================================
 # Devise
-rails_command("generate devise:install")
+# =========================================================
+after_bundle do
+  rails_command("generate devise:install")
+end
 
 environment 'config.action_mailer.default_url_options = { host: "localhost", port: 3000 }', env: 'development'
 
+# =========================================================
 # High Voltage
-file 'app/views/pages/home.html.erb', <<-CODE
-  Hello World!
-CODE
-
+# =========================================================
 initializer 'high_voltage.rb', <<-CODE
   HighVoltage.configure do |config|
     config.home_page = "home"
@@ -73,33 +41,26 @@ initializer 'high_voltage.rb', <<-CODE
   end
 CODE
 
-# Rubocop
-file '.rubocop.yml', <<-CODE
-inherit_gem:
-  rubocop-rails-omakase: rubocop.yml
-CODE
+file 'app/views/pages/home.html.erb'
 
-# Readme
-run "rm README.md"
-
-file 'README.md', <<-CODE
-  "# Hello World!"
-CODE
-
+# =========================================================
 # Database
-rails_command("db:drop")
-rails_command("db:create")
-rails_command("db:migrate")
-rails_command("db:seed")
+# =========================================================
+after_bundle do
+  # rails_command("db:drop")
+  rails_command("db:create")
+  rails_command("db:migrate")
+  rails_command("db:seed")
+end
 
-# Git
+# =========================================================
+# GIT
+# =========================================================
 inject_into_file '.gitignore' do
   "\n# Hidden system files\n.DS_Store"
 end
 
-git add: "."
-git commit: "-a -m 'Initial commit'"
-
-run "code ."
-# run "open http://localhost:3000"
-# run "bin/dev"
+after_bundle do
+  git add: "."
+  git commit: %Q{ -m 'Initial commit' }
+end
