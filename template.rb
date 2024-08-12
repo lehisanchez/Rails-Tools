a19d7a94b5f38d00f24c49b547ee5b1d06c795b9
Lehi Sanchez <17449+lehisanchez@users.noreply.github.com>
2024-08-12 16:09:45 -0400

    Added database time format support

--- template.rb
+++ template.rb
@@ -4,6 +4,7 @@
 gem "devise"
 
 gem_group :development, :test do
+  gem "foreman"
   gem "rspec-rails"
 end
 
@@ -25,6 +26,33 @@ initializer 'enable_uuid.rb',  <<-CODE
   Rails.application.config.generators do |g|
     g.orm :active_record, primary_key_type: :uuid
   end
+CODE
+
+# TIMEZONE
+# initializer 'timestamps.rb', <<-CODE
+#   active_record/connection_adapters/postgresql_adapter.rb"
+#   ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:datetime] = {    name: "timestamptz"  }
+# CODE
+
+initializer 'time_formats.rb', <<-CODE
+  # Define custom date formats
+
+  Date::DATE_FORMATS[:short] = "%b %d, %Y"
+  # Jan 01, 2023
+
+  Date::DATE_FORMATS[:long] = "%A, %B %d, %Y"
+  # Sunday, January 01, 2023
+
+  # Define custom time formats
+
+  Time::DATE_FORMATS[:short] = "%b %d, %Y %I:%M %p"
+  # Jan 01, 2023 03:30 PM
+
+  Time::DATE_FORMATS[:long] = "%A, %B %d, %Y at %I:%M %p"
+  # Sunday, January 01, 2023 at 03:30 PM
+
+  Time::DATE_FORMATS[:nice] = "%b %d, %Y at %I:%M %p"
+  # Jan 01, 2023 at 03:30 PM
 CODE
 
 # =========================================================
@@ -55,6 +83,8 @@ after_bundle do
   # APPLICATION SETTINGS
   # =======================================================
   environment "config.action_mailer.default_url_options = { host: \"localhost\", port: 3000 }", env: "development"
+  environment 'config.active_record.record_timestamps = false'
+  environment 'ActiveRecord::Base.time_zone_aware_types << :timestamptz'
 
   # =======================================================
   # GENERATORS
