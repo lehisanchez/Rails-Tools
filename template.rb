192dbd7764c1f240f24b9832608f79c0877bb345
Lehi Sanchez <sanchez.lehi@gmail.com>
2025-07-26 08:55:04 -0400

    Update template.rb

--- template.rb
+++ template.rb
@@ -5,23 +5,38 @@
 # =========================================================
 
 run 'clear'
-include_auth = false
+
+# =========================================================
+# QUESTIONS
+# =========================================================
+if yes?("Would you like to add Omniauth? (y/n):")
+  skip_authentication = false
+else
+  skip_authentication = true
+end
 
 # =========================================================
 # GEMS
 # =========================================================
-if yes?("Would you like to add Omniauth? (y/n):")
-  include_auth = true
+
+unless skip_authentication
   gem "omniauth"
   gem "omniauth-rails_csrf_protection"
   gem "omniauth-entra-id"
 end
 
 gem "bundler-audit"
-gem "lograge"
+gem "amazing_print"
+gem "rails_semantic_logger"
+gem "prefab-cloud-ruby"
 
 gem_group :development, :test do
   gem "dotenv-rails"
+  gem "rspec-rails", "~> 8.0.0"
+end
+
+gem_group :development do
+  gem "rails_live_reload"
 end
 
 # =========================================================
@@ -30,8 +45,24 @@ end
 remove_file('config/database.yml')
 remove_file('config/credentials.yml.enc')
 remove_file('config/master.key')
+remove_file('readme.md')
 
 # https://anti-pattern.com/strip-leading-whitespace-from-heredocs-in-ruby
+
+# =========================================================
+# .ENV FILES
+# =========================================================
+file '.aprc' do
+  <<-'RUBY'.strip_heredoc
+  AwesomePrint.defaults = {
+    :indent => -2,
+    :color => {
+      :hash  => :pale,
+      :class => :white
+    }
+  }
+  RUBY
+end
 
 file '.env.development' do
   <<-CODE.strip_heredoc
@@ -57,40 +88,75 @@ file '.env.test.local' do
   CODE
 end
 
-unless include_auth == false
+
+# =========================================================
+# AUTHENTICATION .ENV FILES
+# =========================================================
+unless skip_authentication
   append_file '.env.development' do
     <<-CODE.strip_heredoc
-    AUTH_PROVIDER_ID=123456
-    AUTH_PROVIDER_SECRET=123456
-    AUTH_PROVIDER_TENANT_ID=123456
+    AUTH_CLIENT_ID=123456
+    AUTH_CLIENT_SECRET=123456
+    AUTH_TENANT_ID=123456
     CODE
   end
 
   append_file '.env.development.local' do
     <<-CODE.strip_heredoc
-    AUTH_PROVIDER_ID=123456
-    AUTH_PROVIDER_SECRET=123456
-    AUTH_PROVIDER_TENANT_ID=123456
+    AUTH_CLIENT_ID=123456
+    AUTH_CLIENT_SECRET=123456
+    AUTH_TENANT_ID=123456
     CODE
   end
 
   append_file '.env.test' do
     <<-CODE.strip_heredoc
-    AUTH_PROVIDER_ID=123456
-    AUTH_PROVIDER_SECRET=123456
-    AUTH_PROVIDER_TENANT_ID=123456
+    AUTH_CLIENT_ID=123456
+    AUTH_CLIENT_SECRET=123456
+    AUTH_TENANT_ID=123456
     CODE
   end
 
   append_file '.env.test.local' do
     <<-CODE.strip_heredoc
-    AUTH_PROVIDER_ID=123456
-    AUTH_PROVIDER_SECRET=123456
-    AUTH_PROVIDER_TENANT_ID=123456
+    AUTH_CLIENT_ID=123456
+    AUTH_CLIENT_SECRET=123456
+    AUTH_TENANT_ID=123456
     CODE
   end
 end
 
+file 'README.md' do <<-EOF.strip_heredoc
+  # #{app_name.capitalize}
+
+  ## Setup
+
+  1. Pull down the app from version control
+  2. Make sure you have Postgres installed and running
+  3. Run `bin/setup` to install dependencies and set up the database
+  4. Run `bin/ci` to run tests, quality, and security checks
+
+  ## Running The Application
+
+  1. Run `bin/dev` to start the application in development mode
+
+  ## Tests and CI
+
+  1. Run `bin/ci` to run all tests, quality, and security checks
+  2. `tmp/test.log` will use production logging format *not* the development one.
+  3. The vulnerability check output will be in `tmp/brakeman.html`, which can be opened in your browser
+
+  ## Production
+
+  * All runtime configuration should be supplied in the UNIX environment.
+  * Rails logging uses Semantic Logging with Prefab.
+  EOF
+end
+
+
+# =========================================================
+# BIN FILES
+# =========================================================
 file 'bin/ci' do
   <<-'RUBY'.strip_heredoc
   #!/usr/bin/env bash
@@ -111,11 +177,8 @@ file 'bin/ci' do
     fi
   fi
 
-  echo "[ bin/ci ] Running unit tests"
-  bin/rails test
-
-  echo "[ bin/ci ] Running system tests"
-  bin/rails test:system
+  echo "[ bin/ci ] Running tests"
+  bundle exec rspec
 
   echo "[ bin/ci ] Analyzing code for security vulnerabilities."
   echo "[ bin/ci ] Output will be in tmp/brakeman.html, which"
@@ -126,11 +189,77 @@ file 'bin/ci' do
   echo "[ bin/ci ] security vulnerabilities"
   bundle exec bundle audit check --update
 
+  echo "[ bin/ci ] Analyzing code quality with RuboCop"
+  bundle exec rubocop -a
+
   echo "[ bin/ci ] Done"
   RUBY
 end
 
+# =========================================================
+# INITIALIZERS
+# =========================================================
+
+# config/initializers/enable_yjit.rb
+initializer 'enable_yjit.rb' do
+  <<-'RUBY'.strip_heredoc
+  if defined? RubyVM::YJIT.enable
+    Rails.application.config.after_initialize do
+      RubyVM::YJIT.enable
+    end
+  end
+  RUBY
+end
+
+# config/initializers/logging.rb
+initializer 'logging.rb' do
+  <<-'RUBY'.strip_heredoc
+  SemanticLogger.sync!                                  # Use synchronsous processing for targeting logging with current context
+  SemanticLogger.default_level = :trace                 # Prefab will take over the filtering
+  SemanticLogger.add_appender(
+    io: $stdout,                                        # Use whatever appender you like
+    formatter: Rails.env.development? ? :color : :json,
+    filter: Prefab.log_filter,                          # Insert our Prefab filter
+  )
+  RUBY
+end
+
+# config/initializers/omniauth_providers.rb
+unless skip_authentication
+  initializer 'omniauth_providers.rb' do
+    <<-'RUBY'.strip_heredoc
+    Rails.application.config.middleware.use OmniAuth::Builder do
+      provider :developer if Rails.env.development? || Rails.env.test?
+      provider(
+        :entra_id,
+        {
+          client_id:      ENV['AUTH_CLIENT_ID'],
+          client_secret:  ENV['AUTH_CLIENT_SECRET'],
+          tenant_id:      ENV['AUTH_TENANT_ID']
+        }
+      )
+    end
+    RUBY
+  end
+end
+
+
+# =======================================================
+# APP SETTINGS
+# =======================================================
+
 after_bundle do
+  # ===========================
+  # ENVIRONMENT CONFIGURATION
+  # ===========================
+  environment "config.generators.system_tests = nil"  # Disables systems tests
+  environment "config.generators.assets false"        # Disables asset generation during 'rails g scaffold'
+  environment "config.generators.helper false"        # Disables helper
+  environment "config.generators.stylesheets false"   # Disables stylesheets
+
+  # ===========================
+  # .GITIGNORE
+  # ===========================
   append_file '.gitignore' do
     <<-CODE.strip_heredoc
 
@@ -148,19 +277,13 @@ after_bundle do
     .DS_Store
     CODE
   end
-end
 
-# =========================================================
-# INITIALIZERS
-# =========================================================
-
-# YJIT
-initializer 'enable_yjit.rb' do
-  <<-'RUBY'.strip_heredoc
-  if defined? RubyVM::YJIT.enable
-    Rails.application.config.after_initialize do
-      RubyVM::YJIT.enable
-    end
-  end
-  RUBY
+  # ===========================
+  # INSTALLERS
+  # ===========================
+  rails_command("generate rspec:install")
+  rails_command("generate authentication") unless skip_authentication
+  run("bundle install")
+  run("bundle exec bin/setup --skip-server")
+  run("bundle exec bin/ci")
 end
