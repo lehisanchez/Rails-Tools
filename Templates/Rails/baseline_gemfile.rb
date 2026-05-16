8b3db69175b51107993e12b536e9dc48544ee0f5
Lehi Sanchez <sanchez.lehi@gmail.com>
2026-05-16 18:26:43 -0400

    Add maquina-components gem to asset dependencies

--- Templates/Rails/baseline_gemfile.rb
+++ Templates/Rails/baseline_gemfile.rb
@@ -5,6 +5,7 @@ gem "rails", github: "rails/rails", branch: "8-1-stable"
 
 # Assets & front end
 gem "importmap-rails"
+gem "maquina-components"
 gem "propshaft"
 gem "stimulus-rails"
 gem "tailwindcss-rails"
